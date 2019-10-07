d77397c94e4c14fa6524bb45042f5f9e96c96fdb
GlowKasia <gwkasix@gmail.com>
2019-10-07 20:33:52 +0200

    commit

---
+++ src/main/java/pl/sdajava25/securitytemplate/controller/IndexController.java
@@ -0,0 +1,22 @@
+package pl.sdajava25.securitytemplate.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/")
+@AllArgsConstructor
+public class IndexController {
+
+    @GetMapping(path = "/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginForm(){
+        return "login-form";
+    }
+}
